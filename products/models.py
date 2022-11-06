8b5ad520166ba9cc351835c13e8683c23363294f
abbasymot <motameditalemi@gmail.com>
2022-11-06 11:21:55 +0330

    add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1500)
+    score = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def score_average(self):
+        # TODO
+        return self.score
