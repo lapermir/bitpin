a9c10b8665414d4a5ab16b96e623a1140d313eda
abbasymot <motameditalemi@gmail.com>
2022-11-06 11:27:28 +0330

    add product serializer

---
+++ products/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import Product
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description', 'score']
