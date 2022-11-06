43659fcbab19620c15a98a53eda98209f94fbf5e
abbasymot <motameditalemi@gmail.com>
2022-11-06 13:23:21 +0330

    add rate to model

--- products/models.py
+++ products/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+
+class Rate(models.Model):
+    score = models.IntegerField(default=0)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=1500)
-    score = models.IntegerField(default=0)
+    score = models.ForeignKey(
+        Rate, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.title
